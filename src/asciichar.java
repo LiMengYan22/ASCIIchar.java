59358d3d89001c337863fb409a94818422745ac7
LiMengYan <80227002+yanlimeng@users.noreply.github.com>
2021-03-22 11:34:27 +0100

    Add files via upload

---
+++ src/asciichar.java
@@ -0,0 +1,14 @@
+public class asciichar {
+ public static void main(String[] args) {
+      
+   char letra=0;
+   for(int i=0; i<=255 ; i++ )
+        System.out.println("El codigo ASCII de la letra " + 
+                            letra++ + " es " + i);                           
+ }  
+}  
+
+
+
+
+
